fad92b17eeaff3e9f4bd9a042a210912ee6ecca2
Eduardo Nunez <eng91694@gmail.com>
2017-11-30 22:34:50 -0600

    Restrucuting file directories

---
+++ src/common/audiolibrary.cpp
@@ -0,0 +1,7 @@
+#define DB_CONNECTION_STRING "mysql://ld16gcvhnt135b15:r9515p7c6ax4c02m@mna97msstjnkkp7h.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/t5b9qq7rpx44t33l"
+#define DB_HOST "mna97msstjnkkp7h.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"
+#define DB_USERNAME "ld16gcvhnt135b15"
+#define DB_PASSWORD "r9515p7c6ax4c02m"
+#define PORT "3306"
+
+
